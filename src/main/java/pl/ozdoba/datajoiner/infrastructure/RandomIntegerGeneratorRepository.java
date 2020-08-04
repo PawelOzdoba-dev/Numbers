300972498d87e7c0e49d2fdab77da9b1953941fd
Paweł Ozdoba <pawelozdoba07@gmail.com>
2020-08-05 00:27:36 +0200

    Code refactoring.

--- src/main/java/pl/ozdoba/datajoiner/infrastructure/RandomIntegerGeneratorRepository.java
+++ src/main/java/pl/ozdoba/datajoiner/infrastructure/RandomIntegerGeneratorRepository.java
@@ -3,15 +3,14 @@ package pl.ozdoba.datajoiner.infrastructure;
 import pl.ozdoba.datajoiner.domain.DataRepository;
 
 import java.util.Optional;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomIntegerGeneratorRepository implements DataRepository {
-    private static final int MAX_NUMBER = 1000;
-    private Random randomGenerator = new Random(); //TODO to change
+    private static final int MAX_NUMBER = 1001;
 
     @Override
     public Optional getData() {
-        var randomNumber = randomGenerator.nextInt(MAX_NUMBER);
+        var randomNumber = ThreadLocalRandom.current().nextInt(0, MAX_NUMBER);
         return Optional.of(randomNumber);
     }
 }
