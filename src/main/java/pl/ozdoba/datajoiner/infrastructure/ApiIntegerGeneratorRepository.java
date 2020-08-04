4c16563001a175262935fabafdb6864804ad3b1f
Paweł Ozdoba <pawelozdoba07@gmail.com>
2020-08-05 00:35:18 +0200

    Code refactoring.

--- src/main/java/pl/ozdoba/datajoiner/infrastructure/ApiIntegerGeneratorRepository.java
+++ src/main/java/pl/ozdoba/datajoiner/infrastructure/ApiIntegerGeneratorRepository.java
@@ -2,12 +2,10 @@ package pl.ozdoba.datajoiner.infrastructure;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import pl.ozdoba.datajoiner.domain.DataRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 import static java.lang.Integer.valueOf;
